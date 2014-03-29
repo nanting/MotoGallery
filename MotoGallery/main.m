8203a9e45884c01edb797ac1507aee0881664b16
nanting <nantingyang@Nantings-MacBook-Pro-2.local>
2014-03-29 13:23:27 -0700

    Initial Commit

---
+++ MotoGallery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MotoGallery
+//
+//  Created by Nanting Yang on 3/29/14.
+//  Copyright (c) 2014 Nanting Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MGAppDelegate class]));
+    }
+}
